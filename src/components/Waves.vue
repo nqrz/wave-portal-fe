d85a665a313d26c02b598c19f468962da52d8cd4
Nizar Ahmad Baihaqi <nizarbaihaq@gmail.com>
2022-02-10 15:09:58 +0700

    Responsive grid 🎨

--- src/components/Waves.vue
+++ src/components/Waves.vue
@@ -26,6 +26,28 @@ const formatDate = (date) => {
           {{ wave.message }}
         </h4>
       </div>
+      <div v-for="wave in waves" class="card">
+        <h6 style="margin: 0;">
+          {{ formatDate(wave.timestamp) }}
+        </h6>
+        <p style="margin: 0; margin-top: 8px;">
+          Sender : {{ wave.address.slice(0, 14) }}...
+        </p>
+        <h4>
+          {{ wave.message }}
+        </h4>
+      </div>
+      <div v-for="wave in waves" class="card">
+        <h6 style="margin: 0;">
+          {{ formatDate(wave.timestamp) }}
+        </h6>
+        <p style="margin: 0; margin-top: 8px;">
+          Sender : {{ wave.address.slice(0, 14) }}...
+        </p>
+        <h4>
+          {{ wave.message }}
+        </h4>
+      </div>
     </div>
   </div>
 </template>
@@ -37,14 +59,19 @@ const formatDate = (date) => {
   padding: 14px;
   word-wrap: break-word;
   border-radius: 6px;
-  max-width: 675px;
 }
 
 .grid {
-  width: 720px;
-  margin-top: 22px;
-  margin-bottom: 22px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  width: 100%;
+}
+
+@media (min-width: 720px) {
+  .grid {
+    width: 720px;
+    margin-top: 22px;
+    margin-bottom: 22px;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+  }
 }
 </style>
