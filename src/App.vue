e11ec3ef690d8eb2a04c293777ed73ee1f55e841
Nizar Ahmad Baihaqi <nizarbaihaq@gmail.com>
2022-02-10 11:22:55 +0700

    Integrate with the contract 🛠️

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,129 @@
 <script setup>
+import { onMounted, ref } from "vue"
+import { ethers } from "ethers"
+import abi from "./utils/WavePortal.json"
+import Waves from "./components/Waves.vue";
 
+const currentAccount = ref('')
+const allWaves = ref([])
+const message = ref('')
+
+const contractAddress = "0xd010c650A53080244c7812d5bAAA0ED45D58D1C7"
+
+const contractABI = abi.abi;
+
+const getAllWaves = async () => {
+  try {
+    const { ethereum } = window;
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+      const waves = await wavePortalContract.getAllWaves()
+
+      let wavesCleaned = [];
+      waves.forEach(wave => {
+        wavesCleaned.push({
+          address: wave.waver,
+          timestamp: new Date(wave.timestamp * 1000),
+          message: wave.message
+        });
+      });
+
+      allWaves.value = wavesCleaned
+    } else {
+      console.log("Ethereum object doesn't exist!")
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const checkIfWalletIsConnected = async () => {
+  try {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+
+    const accounts = await ethereum.request({ method: "eth_accounts"});
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("found an authorized account:", account);
+      currentAccount.value = account
+      getAllWaves()
+    } else {
+      console.log("No authorized account found")
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const connectWallet = async () => {
+  try {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      alert("Get MetaMask!");
+      return;
+    }
+
+    const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+    console.log("Connected", accounts[0]);
+    currentAccount.value = accounts[0]
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const handleWave = async () => {
+  try {
+    const { ethereum } = window;
+
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+      let count = await wavePortalContract.getTotalWaves();
+      console.log("Retrieved total wave count...", count.toNumber());
+
+      const waveTxn = await wavePortalContract.wave(message.value);
+      console.log("Mining...", waveTxn.hash);
+      message.value = ''
+
+      await waveTxn.wait();
+      console.log("Mined -- ", waveTxn.hash);
+
+      count = await wavePortalContract.getTotalWaves();
+      console.log("Retrieved total wave count..", count.toNumber());
+    } else {
+      console.log("Ethereum object doesn't exist!");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+onMounted(() => {
+  checkIfWalletIsConnected()
+})
 </script>
 
 <template>
-  <div class="tooltip">
-    <p>
-      Learn Web 3 here
-    </p>
-    <a class="link" href="http://buildspace.so" target="_blank" rel="noopener noreferrer">
-      <button class="btn">
-        🦄 Buildspace
-      </button>
-    </a>
-  </div>
-  <div class="container">
+  <a class="tooltip link" href="http://buildspace.so" target="_blank" rel="noopener noreferrer">
+    <button class="btn">
+      🦄 Buildspace
+    </button>
+  </a>
+  <div class="container bg-banner" style="height: 100vh;">
     <figure>
       <img class="image" src="./assets/ijay.jpg" alt="Nizar Baihaqi photo">
     </figure>
@@ -22,19 +132,29 @@
         Nizar Baihaqi &#8599;
       </a>
     </h1>
-    <p>
+    <p style="padding: 14px;">
       Hello folks, I'm Nizar Baihaqi. A Fullstack web dev that always confused which framework to use. 
     </p>
     <p>
       Wave at me and share your thoughts
     </p>
-    <form @submit.prevent="handleWave" class="form-container">
-      <input class="input" type="text" name="message" id="message" placeholder="Message here">
-      <button class="btn" type="submit">
+    <div v-if="!currentAccount" class="form-container">
+      <button @click="connectWallet" class="btn" style="width: 100%;">
+        Connect Wallet
+      </button>
+      <h6 style="color: gray;">You have to connect your wallet to message me.</h6>
+    </div>
+    <form v-else @submit.prevent="handleWave" class="form-container">
+      <input class="input" v-model="message" type="text" name="message" id="message" placeholder="Message here">
+      <button class="btn" style="align-self: end;" type="submit">
         Wave 👋
       </button>
     </form>
+    <div>
+      {{ message }}
+    </div>
   </div>
+  <Waves :waves="allWaves" />
 </template>
 
 <style>
@@ -56,11 +176,13 @@ body {
 }
 
 .container {
-  height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+}
+
+.bg-banner {
   background-image: linear-gradient(to bottom left, whitesmoke, lightblue);
 }
 
@@ -104,7 +226,6 @@ figure {
 }
 
 .btn {
-  align-self: end;
   border: none;
   margin-top: 10px;
   padding: 10px;
---
+++ src/components/Waves.vue
@@ -0,0 +1,50 @@
+<script setup>
+defineProps({
+  waves: Array
+})
+
+const formatDate = (date) => {
+  const options = { year: 'numeric', month: 'long', day: 'numeric' }
+  return new Date(date).toLocaleDateString('en', options)
+}
+</script>
+
+<template>
+  <h2 style="text-align: center;">
+    Waves
+  </h2>
+  <div class="container" style="background-color: aqua;">
+    <div class="grid">
+      <div v-for="wave in waves" class="card">
+        <h6 style="margin: 0;">
+          {{ formatDate(wave.timestamp) }}
+        </h6>
+        <p style="margin: 0; margin-top: 8px;">
+          Sender : {{ wave.address.slice(0, 14) }}...
+        </p>
+        <h4>
+          {{ wave.message }}
+        </h4>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  background-color: aliceblue;
+  margin: 10px;
+  padding: 14px;
+  word-wrap: break-word;
+  border-radius: 6px;
+  max-width: 675px;
+}
+
+.grid {
+  width: 720px;
+  margin-top: 22px;
+  margin-bottom: 22px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+}
+</style>
